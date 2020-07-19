f347210e916f010e3b208e5b5f7478071236732c
Karishma-Joseph <karishmajoseph70@gmail.com>
2020-07-18 21:39:48 -0700

    Set completed to false

--- src/App.js
+++ src/App.js
@@ -1,72 +1,79 @@
-import React, { Component } from 'react';
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
-    todos : [
+    todos: [
       {
         id: 1,
         title: "Take out trash",
-        completed: false
+        completed: false,
       },
       {
         id: 2,
         title: "Dinner with wife",
-        completed: true
+        completed: false,
       },
       {
         id: 3,
         title: "Meeting with boss",
-        completed: false
+        completed: false,
       },
-    ]
-  }
+    ],
+  };
 
   // Toggle completed
   markComplete = (id) => {
-    this.setState({todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-      }) });
-  }
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
 
   //delete
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id) ]
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
     });
-  }
+  };
 
   // Add
   addTodo = (title) => {
     const newTodo = {
       id: 4,
       title,
-      complete: false
-    }
-    this.setState({todos: [...this.state.todos, newTodo]})
-  }
+      complete: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     return (
-     <div className="App">
-       <div className="container">
-          <Header style = {headerStyle} />
-          <AddTodo addTodo = {this.addTodo}/>
-          <Todos todos = {this.state.todos} markComplete = {this.markComplete} delTodo = {this.delTodo} />
-       </div>
-     </div>
+      <div className="App">
+        <div className="container">
+          <Header style={headerStyle} />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
+      </div>
     );
   }
 }
 
 const headerStyle = {
-  backgroundColor: '#333',
-  color: '#fff',
-  textAlign: 'center'
-}
+  backgroundColor: "#333",
+  color: "#fff",
+  textAlign: "center",
+};
 export default App;
